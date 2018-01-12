9eb863385e7f381d929937453d875d79e29de1a8
Web D.va <webNoDva@gmail.com>
2018-01-12 14:59:33 -0500

    Created node backend template for regression calculation API

--- main.js
+++ main.js
@@ -1,7 +1,46 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
+
+/*
+ * API Server
  */
 
+const app = express();
+app.use(cors()); // Needed for file sharing.
+// Needed for POST requests.
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
+const port = process.env.PORT || '3000';
+app.set('port', port);
+
+/*
+ * API Server
+ */
+
+// compute linear regression line
+app.get('/regression/:datapoints', (req, res) => {
+    const dataPoints = req.params.datapoints;
+});
+
+/*
+ * HTTP Server
+ */
+
+// Point static path to dist
+app.use(express.static(path.join(__dirname, '/frontend/dist')));
+
+// Catch all other routes and return the index file
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, '/frontend/dist/index.html'));
+});
+
+// Start the HTTP server.
+app.listen(port, (errorThatOccurs) => {
+    if (errorThatOccurs) {
+        return console.log('Something bad happened.', errorThatOccurs);
+    }
+
+    console.log(`Server running on port ${port}`);
+});
