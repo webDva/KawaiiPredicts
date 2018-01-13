020349b24891b027d776e91e854c53c92bf6c529
Web D.va <webNoDva@gmail.com>
2018-01-13 11:17:58 -0500

    added baseUrl, imported HttpClient, stuff for making a full-stack app

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+// ng build --prod --aot=false
+
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {environment} from '../environments/environment';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app works!';
+export class AppComponent implements OnInit {
+
+    baseUrl = '';
+
+    constructor(private http: HttpClient) {}
+
+    ngOnInit() {
+        if (!environment.production) {
+            this.baseUrl = 'http://localhost:3000';
+        }
+    }
 }
