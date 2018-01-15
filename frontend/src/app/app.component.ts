8e5755d83006a902384dcc5429605d334a7ac00c
Web D.va <webNoDva@gmail.com>
2018-01-15 09:43:54 -0500

    made progress in creating the table of data pairs

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -12,17 +12,37 @@ import {environment} from '../environments/environment';
 export class AppComponent implements OnInit {
 
     baseUrl = '';
-    
+
     dset = '1_8_2_8_3_8_4_3_5_2_6_-2_7_-3_8_-4';
     results = {};
-    
+
+    numPairs = 1;
+    dataPairs = [[1, 2], [3, 4]];
+
     getRegression(dataset: string) {
         this.http.get(this.baseUrl + '/regression/' + dataset).subscribe(data => {
             this.results = data;
         },
-        err => {
-            
-        });
+            err => {
+
+            });
+    }
+
+    // Random float between
+    randomFloatBetween(minValue, maxValue, precision) {
+        if (typeof (precision) == 'undefined') {
+            precision = 2;
+        }
+        return parseFloat(Math.min(minValue + (Math.random() * (maxValue - minValue)), maxValue).toFixed(precision));
+    }
+
+    // generate random dataset values
+    generateData() {
+        this.dataPairs = [];
+
+        for (let i = 0; i < 10; i++) {
+            this.dataPairs.push([this.randomFloatBetween(-100, 100, null), this.randomFloatBetween(-100, 100, null)]);
+        }
     }
 
     constructor(private http: HttpClient) {}
@@ -31,5 +51,7 @@ export class AppComponent implements OnInit {
         if (!environment.production) {
             this.baseUrl = 'http://localhost:3000';
         }
+
+        this.generateData();
     }
 }
