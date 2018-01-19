58233ea24de43506fcb7267da64d8336ae3579d2
Web D.va <webNoDva@gmail.com>
2018-01-19 13:12:04 -0500

    force https for heroku

--- main.js
+++ main.js
@@ -38,6 +38,17 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
+// force https 
+app.use((req, res, next) => {
+    if (process.env.NODE_ENV === 'production') {
+        if (req.headers['x-forwarded-proto'] !== 'https')
+            return res.redirect('https://' + req.headers.host + req.url);
+        else
+            return next();
+    } else
+        return next();
+});
+
 const port = process.env.PORT || '3000';
 app.set('port', port);
 
